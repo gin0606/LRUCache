28431008c3ce0bc7895631e42faf92ab261fc843
gin0606 <kkgn06@gmail.com>
2017-09-28 21:39:24 +0900

    Create xcode project

---
+++ LRUCache/LRUCache.h
@@ -0,0 +1,19 @@
+//
+//  LRUCache.h
+//  LRUCache
+//
+//  Created by gin0606 on 2017/09/28.
+//  Copyright © 2017年 gin0606. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for LRUCache.
+FOUNDATION_EXPORT double LRUCacheVersionNumber;
+
+//! Project version string for LRUCache.
+FOUNDATION_EXPORT const unsigned char LRUCacheVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LRUCache/PublicHeader.h>
+
+
